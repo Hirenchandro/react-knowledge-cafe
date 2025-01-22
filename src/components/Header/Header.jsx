a80bc68ee6cb21f94c949b0b873af06f8093c541
Hiren Chandra <hiren@akijfood.com>
2025-01-22 13:59:02 +0600

    react push

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+import profile from '../../assets/images/profile.png'
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center border-b-2 p-4'>
+             <h1 className='text-6xl'>Knowledge Cafe </h1>
+             <img src={profile} alt="" />
+        </header>
+        
+    );
+};
+
+export default Header;
